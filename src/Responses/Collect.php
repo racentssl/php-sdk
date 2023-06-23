7cf790743966ef179d3488989472a7b4d17272f4
Bruce Lam <xiaohuilam@users.noreply.github.com>
2023-06-23 08:10:43 +0800

    Update Collect.php

--- src/Responses/Collect.php
+++ src/Responses/Collect.php
@@ -10,7 +10,14 @@ namespace Racent\Responses;
  * @property string $endDate "2020-03-22 07:59:58"
  * @property string $certificate
  * @property string $caCertificate
- * @property string $status
+ * @property string $status 'PENDING'|'COMPLETE'|'CANCELLED'
+ * @property string $DCVfileName 文件验证文件名
+ * @property string $DCVfileContent 文件验证文件内容
+ * @property string $DCVdnsHost Dns验证主机值
+ * @property string $DCVdnsValue Dns验证记录值
+ * @property string $DCVdnsType Dns验证记录类型
+ * @property string $DCVfilePath 文件验证文件路径
+ * @property SslPlace\DcvInfo $dcvInfo 申请证书中的每个域名验证值不一样时，此项为Y
  */
 class Collect
 {
