a78f5e822d6833f8bb0899082784991e7bd389ee
Bruce Lam <xiaohuilam@users.noreply.github.com>
2023-06-23 08:05:43 +0800

    Create DcvInfoItem.php

---
+++ src/Responses/SslPlace/DcvInfoItem.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Racent\Responses\SslPlace;
+
+/**
+ * @property string $DCVfileName 文件验证文件名
+ * @property string $DCVfileContent 文件验证文件内容
+ * @property string $DCVdnsHost Dns验证主机值
+ * @property string $DCVdnsValue Dns验证记录值
+ * @property string $DCVdnsType Dns验证记录类型
+ * @property string $DCVfilePath 文件验证文件路径
+ */
+class DcvInfoItem
+{
+}
