b3ded7de35fe6c3c57a70a159a92943735f1b16a
Bruce Lam <xiaohuilam@users.noreply.github.com>
2023-12-19 23:55:05 +0800

    Update config.php

--- src/config.php
+++ src/config.php
@@ -2,6 +2,11 @@
 
 return [
     'racent' => [
-        'api_token' => env('RACENT_APITOKEN'),
+        'api_token' => env('RACENT_API_TOKEN'),
+        'api_url' => env('RACENT_API_URL', null),
+    ],
+    'nicsrs' => [
+        'api_token' => env('NICSRS_API_TOKEN'),
+        'api_url' => env('NICSRS_API_URL', null),
     ],
 ];
