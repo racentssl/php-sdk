46b67e19ac45003c6fee15377502bf8eb710deb3
Bruce Lam <xiaohuilam@users.noreply.github.com>
2023-12-19 23:53:55 +0800

    Update RacentServiceProvider.php

--- src/RacentServiceProvider.php
+++ src/RacentServiceProvider.php
@@ -9,11 +9,11 @@ class RacentServiceProvider extends ServiceProvider
     public function register()
     {
         app()->singleton('racent', function () {
-            return new Racent(config('supplier.racent.api_token'));
+            return new Racent(config('supplier.racent.api_token'), config('supplier.racent.api_url', null));
         });
 
         app()->singleton('nicsrs', function () {
-            return new NicSRS(config('supplier.nicsrs.api_token'));
+            return new NicSRS(config('supplier.nicsrs.api_token'), config('supplier.nicsrs.api_url', null));
         });
 
         $this->mergeConfigFrom(__DIR__ . '/config.php', 'supplier');
